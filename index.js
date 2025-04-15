5103ff07cbae081c2e10886c299d4eb756323cd2
fc <fc@test.myguest.virtualbox.org>
2025-04-15 20:53:52 -0300

    Chore: readme initiated

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 import { spawn } from "child_process";
 import { argv } from "process";
+import fs from 'fs';
+
+const PATH = "~/dev/projetos/";
 
 const exec = (project) => {
-    spawn(`vim`, [`~/dev/projetos/${project}/`],{stdio: 'inherit'});
+    spawn(`vim`, [`${PATH}${project}/`], {stdio: 'inherit'});
 };
 
 // O parametro é a partir do 2
@@ -11,3 +14,18 @@ const exec = (project) => {
 if (argv[2] == 'go') {
     exec(argv[3]);
 };
+
+// const checkDir = (project) => {
+//     fs.readdir(PATH+project, (err, files) => {
+//         if (err) {
+//             console.error("Erro: Diretório não disponível ou encontrado! \n\n[LOG]: " + err);
+//             return;
+//         }
+
+//         files.forEach(file => {
+//             console.log(file);
+//         });
+//     });
+// };
+
+// checkDir(argv[2]);
